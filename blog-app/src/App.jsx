485d15d10c86a21047df5243d23085792b2974bd
CorreaBrunoMiguel <bruno_miguel-correa@etudante.sesisenai.org.br>
2025-09-15 20:56:05 -0300

    pausa 24:02

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
-  return (
-    <div className='text-2xl text-red-500'>
-      APP
-    </div>
-  )
-}
+  return <div className="text-2xl text-red-500">APP</div>;
+};
 
-export default App
+export default App;
